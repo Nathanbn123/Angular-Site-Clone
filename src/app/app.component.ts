e5510956d7d5be687ad1b1199a9af487ee136a39
Nathan Nielson <nathanbn123@gmail.com>
2019-03-29 13:44:53 -0700

    slowly setup necessary js to iterate flag objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Flag } from '../models/flag.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/flags/flags.component.ts
+++ src/app/flags/flags.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+  import { Flag } from './models/flag.model';
 @Component({
   selector: 'app-flags',
   templateUrl: './flags.component.html',
   styleUrls: ['./flags.component.css']
 })
-export class FlagsComponent implements OnInit {
+export class FlagsComponent {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+
 
 }
