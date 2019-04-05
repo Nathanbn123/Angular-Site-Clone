d5347883febe81ea19c624a8321e24b9fd85a37d
Nathan Nielson <nathanbn123@gmail.com>
2019-04-05 10:37:04 -0700

    attempt to establish connection to database

--- src/app/flags/flags.component.ts
+++ src/app/flags/flags.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-  import { Flag } from '../models/flag.model';
+import { Flag } from '../models/flag.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-flags',
   templateUrl: './flags.component.html',
   styleUrls: ['./flags.component.css']
 })
 export class FlagsComponent {
-
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+  this.Flags = database.list('Flags');
+}
 
 
 
