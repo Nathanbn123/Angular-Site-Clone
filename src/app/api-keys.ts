a2d54a7c7b81e29a06810a1906f283198a728583
Nathan Nielson <nathanbn123@gmail.com>
2019-04-05 08:42:50 -0700

    clone down project and integrate firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,9 @@
+
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyDp2dBWhhpDfd_G7iQYYTy5O09fLkTnCIs",
+    authDomain: "virginmobileclone.firebaseapp.com",
+    databaseURL: "https://virginmobileclone.firebaseio.com",
+    projectId: "virginmobileclone",
+    storageBucket: "virginmobileclone.appspot.com",
+    messagingSenderId: "727528735209"
+  };
