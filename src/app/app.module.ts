3ef4fb61e35b880fa92036023212b150155c1bb7
Nathan Nielson <nathanbn123@gmail.com>
2019-04-05 09:34:05 -0700

    succesfully create firebase database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FlagsComponent } from './flags/flags.component';
 import { FooterComponent } from './footer/footer.component';
+
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -17,6 +28,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
